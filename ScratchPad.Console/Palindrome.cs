e6f35a872d8889304528490c081006038901d6e2
Brad Pyne <brpyne@gmail.com>
2023-08-09 11:13:18 -0400

    Palindrome solution

--- ScratchPad.Console/Palindrome.cs
+++ ScratchPad.Console/Palindrome.cs
@@ -3,7 +3,22 @@ namespace Palindrome;
 public class Solution {
     public bool IsPalindrome(int x) {
         
+        var stack = new Stack<char>();
+        
+        var chars = x.ToString().ToCharArray();
+        var charsReverse = x.ToString().ToCharArray();
+        Array.Reverse<char>(charsReverse);
 
-        return true;
+        for(int i = 0; i < chars.Count(); i++)
+        {
+            stack.Push(charsReverse[i]);
+
+            if(chars[i] == charsReverse[i])
+            {
+                stack.Pop();
+            }
+        }
+
+        return stack.Count == 0;
     }
 }
