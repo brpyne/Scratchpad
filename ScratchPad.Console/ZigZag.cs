b029ac07824ff1876b995d55746eaa48fd7ace70
Brad Pyne <brpyne@gmail.com>
2023-08-10 17:04:51 -0400

    ZigZag

--- ScratchPad.Console/ZigZag.cs
+++ ScratchPad.Console/ZigZag.cs
@@ -1,12 +1,69 @@
 namespace ZigZag;
 
-public class Solution {
-    public string Convert(string s, int numRows) {
-        return "11" + Environment.NewLine + "2 ";
-        return string.Empty;
+public class Solution
+{
+    public string Convert(string s, int numRows)
+    {
         // use 2d array of chars
         // rows = param
-        // columns = ? row - (s.Length)
-        // zigzaglength = ?
+        // columns = (rows / n) + (rows % n)
+        // d = row - 2
+
+        // loop columns
+        // if hit row=max
+        //traverse diagnol
+        //if hit row=0
+        // break
+        var n = s.Length;
+        int numColumns = (numRows / n) + (numRows % n);
+        int numDiagnol = n - 2;
+        var resultGrid = new char[numColumns, numRows];
+
+        int index = 0;
+        int column = 0;
+        while (index < n - 1)
+        {
+            // columns
+            for (int r = 0; r < numRows; r++)
+            {
+                if(index != n)
+                {
+                    resultGrid[column, r] = s[index];
+                    index++;
+                }
+
+                if (r == numRows - 1)
+                {
+                    Console.WriteLine("d");
+                    break;
+                }
+
+            }
+
+            // diagnols
+            for (int i = numRows - 2; i > 0; i--)
+            {
+                column++;
+
+                if(index != n)
+                {
+                    resultGrid[column, i] = s[index];
+                    index++;
+                }
+            }
+        }
+
+
+
+        string output = string.Empty;
+        for (int k = 0; k < resultGrid.GetLength(1); k++)
+        {
+            for (int l = 0; l < resultGrid.GetLength(0); l++)
+                output += resultGrid[k, l];
+
+            output += Environment.NewLine;
+        }
+
+        return output;
     }
 }
--- ScratchPad.Tests/ZigZagTests.cs
+++ ScratchPad.Tests/ZigZagTests.cs
@@ -7,7 +7,8 @@ public class ZigZagTests
     [Theory]
     [InlineData("121", 1, "121")]
     [InlineData("121", 2, "11\r\n2 ")]
-    public void IsPalindrome_HappyPath(string s, int rows, string expectedOutput)
+    [InlineData("123456", 3, "1  5\r\n246\r\n3  ")]
+    public void Convert_HappyPath(string s, int rows, string expectedOutput)
     {
         var output = new Solution().Convert(s, rows);
 
